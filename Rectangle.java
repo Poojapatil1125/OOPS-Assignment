38072e6851b2b8671b3b84d57979dba6e95f35bd
Poojapatil1125 <112397052+Poojapatil1125@users.noreply.github.com>
2022-09-15 14:17:48 +0530

    Delete Rectangle.java

--- Rectangle.java
+++
@@ -1,9 +0,0 @@
-package com.Question4;
-
-public class Rectangle extends Shape{
-  public void area(int height,int width)
-  {
-	  int area=height*width;
-	  System.out.println("area of rectangle is "+area);
-  }
-}
