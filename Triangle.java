9273c814127cdfe5d8fb22ee8d4da03a0229e8c0
Poojapatil1125 <112397052+Poojapatil1125@users.noreply.github.com>
2022-09-15 14:15:40 +0530

    Delete Triangle.java

--- Triangle.java
+++
@@ -1,9 +0,0 @@
-package com.Question4;
-
-public class Triangle extends Shape{
-  public void area(int height,int width)
-  {
-	  double area=(height*width)/2;
-	  System.out.println("area of triangle is "+area);
-  }
-}
